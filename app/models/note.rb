841ea67e4a070e5f703c1ab514ca838f6accff80
Nigel Chapman <nigel@macavon.co.uk>
2019-07-21 13:39:59 +0100

    Note model with rich text body

---
+++ app/models/note.rb
@@ -0,0 +1,3 @@
+class Note < ApplicationRecord
+  has_rich_text :body
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_21_122822) do
+ActiveRecord::Schema.define(version: 2019_07_21_123207) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,12 @@ ActiveRecord::Schema.define(version: 2019_07_21_122822) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
